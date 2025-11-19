98dc91cc848256b70f86b6cd6358c5cea4f58335
OBlas <blas0123.22@gmail.com>
2025-11-18 23:40:13 -0500

    Update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { componentTagger } from 'lovable-tagger'
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/smartcollege/",',
   plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   server: {
     host: '::',
